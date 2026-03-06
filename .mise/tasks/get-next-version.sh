1a87c970aab3ff32ca959ecc547c50394e01bd03
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2026-03-06 12:27:38 -0500

    ci: refactor ci workflow to orchestrate others

--- .mise/tasks/get-next-version.sh
+++ .mise/tasks/get-next-version.sh
@@ -10,22 +10,41 @@
 NEXT_VERSION=$(cog bump --auto --dry-run --skip-untracked)
 CALLING_BRANCH=$1
 
-if [ "$NEXT_VERSION" == "" ]; then
-    echo "Bump-able commits found."
-    echo "There will be a next version."
-    echo "new_release=true" >>"$GITHUB_OUTPUT"
-    echo "new_release_version=${NEXT_VERSION}" >>"$GITHUB_OUTPUT"
-else
-    echo "No bump-able commits found"
-    echo "There will be no next version."
-    echo "new_release=false" >>"$GITHUB_OUTPUT"
-    echo "new_release_version=''" >>"$GITHUB_OUTPUT"
-fi
-
-if [ "$CALLING_BRANCH" == "next" ]; then
+case "$CALLING_BRANCH" in
+next)
     echo "called from 'next' branch"
     echo "prerelease=true" >>"$GITHUB_OUTPUT"
-else
+    if [ "$NEXT_VERSION" != "" ]; then
+        echo "Bump-able commits found."
+        echo "There will be a next version."
+        echo "new_release=true" >>"$GITHUB_OUTPUT"
+        echo "new_release_version=${NEXT_VERSION}" >>"$GITHUB_OUTPUT"
+    else
+        echo "No bump-able commits found"
+        echo "There will be no next version."
+        echo "new_release=false" >>"$GITHUB_OUTPUT"
+        echo "new_release_version=''" >>"$GITHUB_OUTPUT"
+    fi
+    ;;
+main)
     echo "called from 'main' branch"
     echo "prerelease=false" >>"$GITHUB_OUTPUT"
-fi
+    if [ "$NEXT_VERSION" != "" ]; then
+        echo "Bump-able commits found."
+        echo "There will be a next version."
+        echo "new_release=true" >>"$GITHUB_OUTPUT"
+        echo "new_release_version=${NEXT_VERSION}" >>"$GITHUB_OUTPUT"
+    else
+        echo "No bump-able commits found"
+        echo "There will be no next version."
+        echo "new_release=false" >>"$GITHUB_OUTPUT"
+        echo "new_release_version=''" >>"$GITHUB_OUTPUT"
+    fi
+    ;;
+*)
+    echo "Not on 'main' or 'next' branch. No release necessary"
+    echo "new_release=false" >>"$GITHUB_OUTPUT"
+    echo "new_release_version=''" >>"$GITHUB_OUTPUT"
+    echo "prerelease=false" >>"$GITHUB_OUTPUT"
+    ;;
+esac
