3b865abe6d113ec3d1854278fcab65b2a6b995b6
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2026-03-06 11:59:27 -0500

    ci: add a next-release-version check

--- .mise/tasks/get-next-version.sh
+++ .mise/tasks/get-next-version.sh
@@ -7,13 +7,17 @@
 
 #MISE description="Dry-run semantic-release to see whether we are doing a release"
 
-semantic-release \
-    --dry-run \
-    --plugins semantic-release-export-data \
-    --verify-conditions semantic-release-export-data \
-    --verify-release '' \
-    --generate-notes semantic-release-export-data \
-    --prepare '' \
-    --publish '' \
-    --success '' \
-    --fail ''
+NEXT_VERSION=$(cog bump --auto --dry-run --skip-untracked)
+
+if [ "$NEXT_VERSION" == "" ]; then
+    echo "Bump-able commits found."
+    echo "There will be a next version."
+    echo "new_release=true" >>"$GITHUB_OUTPUT"
+    echo "new_release_version=${NEXT_VERSION}" >>"$GITHUB_OUTPUT"
+else
+    echo "No bump-able commits found"
+    echo "There will be no next version."
+    echo "new_release=false" >>"$GITHUB_OUTPUT"
+    echo "new_release_version=''" >>"$GITHUB_OUTPUT"
+
+fi
